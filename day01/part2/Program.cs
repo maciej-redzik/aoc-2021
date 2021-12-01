61bf4ae447000667c0871daaef1921a6fa1da95f
Maciej Redzik <maciej.redzik@code-solutions.pl>
2021-12-01 15:28:30 +0100

    Reorganize for part1 & part2

---
+++ day01/part2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = System.IO.File.ReadAllText("input.txt");
+            int []depths = output.Split(Environment.NewLine, int.MaxValue, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int a = 0;
+            int b = 0;
+            int totalIncreased = 0;
+            for(int i = 1 ; i < depths.Length ; i++)
+            {
+                a = depths[i-1];
+                b = depths[i];
+                bool increased = b > a;
+                if(increased)
+                    totalIncreased++;
+
+                Console.WriteLine($"{a} {b} {(increased ? "(increased)" : "(decreased)")}");
+            }
+
+            Console.Out.WriteLine($"Total increased: {totalIncreased}");
+        }
+    }
+}
