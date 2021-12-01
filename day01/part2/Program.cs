dd60ddaac862e9673d9252350c0df78436c6a162
Maciej Redzik <maciej.redzik@code-solutions.pl>
2021-12-01 15:41:11 +0100

    Day 1 - part 2

--- day01/part2/Program.cs
+++ day01/part2/Program.cs
@@ -11,16 +11,30 @@ namespace day01
             int []depths = output.Split(Environment.NewLine, int.MaxValue, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
             int a = 0;
             int b = 0;
+            int c = 0;
             int totalIncreased = 0;
-            for(int i = 1 ; i < depths.Length ; i++)
+            int prevSum = 0;
+            for(int i = 2 ; i < depths.Length ; i++)
             {
-                a = depths[i-1];
-                b = depths[i];
-                bool increased = b > a;
+                a = depths[i-2];
+                b = depths[i-1];
+                c = depths[i];
+                int sum = a + b + c;
+                
+                if(prevSum == 0) {
+                    prevSum = sum;
+                    Console.WriteLine($"{sum} (N/A - no previous sum)");
+                    continue;
+                    
+                }
+                bool increased = sum > prevSum;
                 if(increased)
+                {
                     totalIncreased++;
+                }
+                Console.WriteLine($"{sum} {(increased ? "(increased)" : "(decreased)")}");
 
-                Console.WriteLine($"{a} {b} {(increased ? "(increased)" : "(decreased)")}");
+                prevSum = sum;
             }
 
             Console.Out.WriteLine($"Total increased: {totalIncreased}");
