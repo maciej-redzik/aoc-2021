5934425cbb3c32950d657af5b97912cb68c9fd55
Maciej Redzik <maciej.redzik@code-solutions.pl>
2021-12-03 14:28:45 +0100

    Day 2 - part 1

---
+++ day02/part1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = System.IO.File.ReadAllText("input.txt");
+            string [] commands = output.Split(Environment.NewLine, int.MaxValue, StringSplitOptions.RemoveEmptyEntries);
+
+            int xPos = 0; //horizontal position
+            int yPos = 0; //depth
+
+            for(int i = 0; i < commands.Length; i++)
+            {
+                string commandType = commands[i].Split(' ', StringSplitOptions.None)[0];
+                int value = Convert.ToInt32(commands[i].Split(' ', StringSplitOptions.None)[1]);
+
+                if(commandType == "forward")
+                {
+                    xPos += value;
+                }
+                if(commandType == "up")
+                {
+                    yPos -= value;
+                }
+                if(commandType == "down")
+                {
+                    yPos += value;
+                }
+            }
+
+            Console.WriteLine($"Horizontal position: {xPos}");
+            Console.WriteLine($"Depth: {yPos}");
+
+            Console.WriteLine($"Depth x Horizontal position = {(xPos * yPos)}");
+        }
+    }
+}
